de410702052abb301d1d31325a95317a8ea143c7
Mauro notebook <exalted.archer@gmail.com>
2023-06-08 18:03:38 -0300

    atlas conectado

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectToDatabase = async () =>{
+    const url ='mongodb+srv://mauPineyro:mongoClusterMP1Nomehable1@clustermp1.yuubkwb.mongodb.net/'
+    try {
+        await mongoose.connect(url,
+            { useUnifiedTopology: true, 
+            useNewUrlParser: true });
+        console.log('Conexión a db exitosa');
+    } 
+    catch (err) {
+        return console.log(err);
+    }
+    }
+
+export default connectToDatabase;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 //Práctica Integradora 1
+
+//imports
+import express from "express";
+import connectToDatabase from "./db.js";
+
+//Set
+const app = express();
+const PORT = 8080;
+connectToDatabase();
+
+
+
+//listen
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);  
+   
+})
+
