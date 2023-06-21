8ce75d9b535f71351576040a72c01809d3379dca
Mauro notebook <exalted.archer@gmail.com>
2023-06-21 01:18:12 -0300

    sweet alert fallando, revisar

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,3 +1,19 @@
 //este es el chat.js de cliente
 
 const socket = io()
+let user; 
+Swal.fire({
+  title: "Identificate",
+  input:"text",
+  text: "Por favor ingresa tu nombre..",
+inputValidator: (value)=>{
+  return !value && "¡Necesitas escribir un nombre de usuario para continuar"
+},
+allowOutsideClick:false //impide que el usuario salga de la alerta 
+
+})
+.then(result=>{
+  user = result.value;
+  console.log(user);
+  //Una vez que el usuario se identifica, lo asignamos a la variable user.
+})
--- src/sockets/socketChat.js
+++ src/sockets/socketChat.js
@@ -1,9 +1,12 @@
 //el chat de servidor
-
+import messageModel from "../dao/models/messages.model.js";
 
 //estructurar el socket de chat
 const socketChat = async(io) =>{
-    
+    let historial = [];
+    io.on ('connection', (socket)=>{
+        
+    })
 }
 
 export default socketChat;
