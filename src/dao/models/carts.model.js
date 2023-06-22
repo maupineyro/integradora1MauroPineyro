9e77e4753fc25a961b13c5a37c652206986d4b69
Mauro notebook <exalted.archer@gmail.com>
2023-06-22 18:54:09 -0300

    cartSchema revisado

--- src/dao/models/carts.model.js
+++ src/dao/models/carts.model.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 
 const cartsSchema = new mongoose.Schema ({
     //el id de carrito lo genera mongo
-    products:[
-        {
-            product: String,
-            quantity: Number,
+    products:{
+        type: [
+            {
+            product:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product'
+              }
+            }
+          ]  
         }
-    ]
 });
 
 const cartModel = mongoose.model ('cart',cartsSchema);
