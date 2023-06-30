b54850b4a99e0d8daa46c9205536ca8b3d1991f8
Mauro notebook <exalted.archer@gmail.com>
2023-06-30 15:26:04 -0300

    algunos métodos de userManager creados

--- src/routes/sessions.route.js
+++ src/routes/sessions.route.js
@@ -3,4 +3,15 @@ import { Router } from "express";
 const sessionRouter = Router();
 
 
+sessionRouter.post('/register', async (req,res)=>{
+    try {
+        const {email, password} = req.body;
+        const role = (email === 'adminCoder@coder.com' && password === admin2023) ? 'admin' : 'user';
+        if(!email || !password) return res.send ({status:error, message:'todos los campos son requeridos'});
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 export default sessionRouter;
