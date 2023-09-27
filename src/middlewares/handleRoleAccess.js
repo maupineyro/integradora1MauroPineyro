fdbbfb642c90a848688280127cf6468326e22f60
Mauro notebook <exalted.archer@gmail.com>
2023-09-26 23:12:41 -0300

    middle premium, mejorar

--- src/middlewares/handleRoleAccess.js
+++ src/middlewares/handleRoleAccess.js
@@ -1,3 +1,15 @@
-export const handleRoleAccess = accessEndpoints => (req, res, next) => {
+export const handleRoleAccess = accessPolicies => (req, res, next) => {
+    if (accessPolicies[0] === 'PUBLIC') 
+    return next();
+
+  if (!req.session || !req.session.user) {
+    return res.status(401).json({ status: 'error', error: 'User not authenticated.' });
+  }
+
+  const CurrentUser = req.session.user;
+
+  if (!accessPolicies.includes(CurrentUser.role.toUpperCase())) {
+    return res.status(403).json({ status: 'error', error: 'User without permissions' });
+  }
     next()
 }
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,14 +2,18 @@
 
 import { Router } from "express";
 import { productController } from "../controllers/products.controller.js";
+import { handleRoleAccess } from "../middlewares/handleRoleAccess.js";
 const productRouter = Router();
+
+// Políticas de acceso permitidas
+const allowedPolicies = ['PUBLIC', 'ADMIN', 'PREMIUM'];
 
 //Product Endpoints
 productRouter.get('/', productController.getAll ) //get
 
 productRouter.get('/:pid', productController.getProduct) //get by ID
 
-productRouter.post ('/',productController.add ) //post
+productRouter.post ('/',handleRoleAccess(['ADMIN', 'PREMIUM']),productController.add ) //post
 
 productRouter.delete ('/:pid', productController.delete ) //delete ny ID
 
